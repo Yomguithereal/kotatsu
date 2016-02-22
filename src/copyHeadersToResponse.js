fdeb52ceb34a602a4ea15f8a8180ac11fc1e7416
Yomguithereal <guillaumeplique@gmail.com>
2016-02-22 12:11:39 +0100

    Linting @christianalfoni

--- src/copyHeadersToResponse.js
+++ src/copyHeadersToResponse.js
@@ -1,5 +1,5 @@
-module.exports = function (proxyRes, req, res) {
+module.exports = function(proxyRes, req, res) {
   Object.keys(proxyRes.headers).forEach(function (key) {
     res.append(key, proxyRes.headers[key]);
   });
-}
+};
